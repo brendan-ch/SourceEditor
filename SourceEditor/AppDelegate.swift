ca25e0ebf587637ef92fa4d86e5d8f75ef8ef6b0
Brendan Chen <me@bchen.dev>
2024-02-08 18:15:25 -0800

    add file opening

--- SourceEditor/AppDelegate.swift
+++ SourceEditor/AppDelegate.swift
@@ -25,6 +25,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
-
+    @IBAction func openDocument(_ sender: Any) {
+        // Let the user open a new text document
+        let openPanel = NSOpenPanel()
+        
+        // Set options
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        openPanel.canCreateDirectories = true
+        openPanel.canChooseFiles = true
+        openPanel.allowedContentTypes = [.plainText]
+        
+        // Handle opening of the panel
+        openPanel.begin { result in
+            if result == NSApplication.ModalResponse.OK {
+                if let selectedPath = openPanel.url {
+                    print(selectedPath)
+                }
+            }
+        }
+    }
 }
 
