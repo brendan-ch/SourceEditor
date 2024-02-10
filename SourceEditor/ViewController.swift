65d8ea33bc86c9f09cf425eb38e35264300e034c
Brendan Chen <me@bchen.dev>
2024-02-09 17:14:46 -0800

    remove handleDocumentOpen method

--- SourceEditor/ViewController.swift
+++ SourceEditor/ViewController.swift
@@ -17,10 +17,6 @@ class ViewController: NSViewController, NSTextViewDelegate {
         
         textView.font = monoFont
         textView.enabledTextCheckingTypes = 0
-
-        // Do any additional setup after loading the view.
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(handleDocumentOpen(notification:)), name: .didOpenDocument, object: nil)
     }
 
     override var representedObject: Any? {
@@ -28,17 +24,5 @@ class ViewController: NSViewController, NSTextViewDelegate {
         // Update the view, if already loaded.
         }
     }
-    
-    @objc func handleDocumentOpen(notification: Notification) {
-        if let path = notification.userInfo?["path"] as? URL {
-            // Load the document from the path
-            if let contents = try? String(contentsOf: path) {
-                print(contents)
-                textView.string = contents
-            }
-        }
-    }
-
-
 }
 
