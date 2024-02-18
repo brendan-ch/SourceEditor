4ee36c311087f32d3f2253abac79a97cb08230b8
Brendan Chen <me@bchen.dev>
2024-02-17 17:04:13 -0800

    move view initialization logic to the view controller (lol)

--- SourceEditor/ViewController.swift
+++ SourceEditor/ViewController.swift
@@ -9,14 +9,30 @@ import Cocoa
 
 let monoFont = NSFont(name: "SF Mono", size: 12)
 
-class ViewController: NSViewController, NSTextViewDelegate {
+class ViewController: NSViewController {
     @IBOutlet var textView: NSTextView!
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+        // Stylize the text view
+        textView.font = monoFont
+        textView.enabledTextCheckingTypes = 0
+        textView.delegate = self
+        
+        // Open and initialize the document from the window controller
+        if let document = self.view.window?.windowController?.document as? TextDocument {
+            if document.didReadData {
+                guard let content = representedObject as? DocumentContents else { return }
+                textView.string = content.content
+                
+                document.didReadData = false
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        textView.font = monoFont
-        textView.enabledTextCheckingTypes = 0
     }
 
     override var representedObject: Any? {
@@ -26,3 +42,10 @@ class ViewController: NSViewController, NSTextViewDelegate {
     }
 }
 
+extension ViewController: NSTextViewDelegate {
+    func textViewDidChangeSelection(_ notification: Notification) {
+        // Save the represented object using data from the text view
+        // May need to change the string contents into a class property
+        (representedObject as? DocumentContents)?.content = textView.string
+    }
+}
