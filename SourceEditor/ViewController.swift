c8008a1f42c32ca792975a9d4959a86cf8648ceb
Brendan Chen <me@bchen.dev>
2024-02-17 17:13:50 -0800

    add additional ViewController comments

--- SourceEditor/ViewController.swift
+++ SourceEditor/ViewController.swift
@@ -18,14 +18,23 @@ class ViewController: NSViewController {
         // Stylize the text view
         textView.font = monoFont
         textView.enabledTextCheckingTypes = 0
+        
+        // Set the delegate of the text view to the class
         textView.delegate = self
         
-        // Open and initialize the document from the window controller
+        // Open and initialize the document
+        // The windowController.document property is set when the NSDocument instance calls addWindowController
+        // (see TextDocument.swift line 43)
         if let document = self.view.window?.windowController?.document as? TextDocument {
+            // Check whether data has been read in correctly
             if document.didReadData {
+                // Check that the content's been initialized
                 guard let content = representedObject as? DocumentContents else { return }
+                
+                // Initialize the textView's string contents
                 textView.string = content.content
                 
+                // Reset the state of the document
                 document.didReadData = false
             }
         }
@@ -43,6 +52,8 @@ class ViewController: NSViewController {
 }
 
 extension ViewController: NSTextViewDelegate {
+    // Separate text delegate logic into an extension of ViewController
+    
     func textViewDidChangeSelection(_ notification: Notification) {
         // Save the represented object using data from the text view
         // May need to change the string contents into a class property
