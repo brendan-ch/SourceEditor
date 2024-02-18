990d46bca99a27e52bbc6bdfe1a2ea2f9b1e777e
Brendan Chen <me@bchen.dev>
2024-02-17 17:04:24 -0800

    remove openDocument and saveDocument actions

--- SourceEditor/AppDelegate.swift
+++ SourceEditor/AppDelegate.swift
@@ -24,45 +24,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     // MARK: - IBAction functions
-    
-    /// Open a new document using the document controller.
-    /// @param sender The sender of the IBAction.
-    @IBAction func openDocument(_ sender: Any) {
-        // Get the shared document controller
-        let documentController = NSDocumentController.shared
-        
-        // Open the macOS file picker
-        // Allow for selection of multiple files
-        documentController.beginOpenPanel { urls in
-            // Grab the URLs returned from the file picker
-            guard let urls = urls else { return }
-            
-            // Open each document selected
-            for url in urls {
-                // Attempt to open the document
-                documentController.openDocument(withContentsOf: url, display: true) { document, alreadyOpen, error in
-                    if let error = error {
-                        NSApp.presentError(error)
-                    }
-                }
-            }
-        }
-    }
-    
-    /// Save the document to the document's original file path.
-    /// @param sender The sender of the IBAction.
-    @IBAction func saveDocument(_ sender: Any) {
-        print(type(of: sender))
-        // Attempt to save the document using the shared document controller.
-        let documentController = NSDocumentController.shared
-        
-        if let currentDocument = documentController.currentDocument as? TextDocument {
-            // Attempt to save the current document
-            currentDocument.save(to: currentDocument.fileURL!, ofType: currentDocument.fileType!, for: .saveOperation) { error in
-                if let error = error {
-                    NSApp.presentError(error)
-                }
-            }
-        }
-    }
 }
